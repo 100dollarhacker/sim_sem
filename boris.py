9d3d8fd81722ebeae0f5d354738cf4bbfd9aa336
Eduard Sinelnikov <100dollarhacker@gmail.com>
2026-06-24 16:53:39 -0400

    now with real values

--- boris.py
+++ boris.py
@@ -1,10 +1,10 @@
 import numpy as np
 from plasmapy.simulation.particle_integrators import BorisIntegrator
 
-B = np.array([[0.0, 0.0, 5.0]])
+B = np.array([[5.0e-8, 0.0, 0.0]])
 E = np.array([[0.0, 0.0, 0.0]])
 x_t0 = np.array([[0.0, 0.0, 0.0]])
-v_t0 = np.array([[5.0, 0.0, 0.0]])
-for n in range(200):
-    x_t0, v_t0 = BorisIntegrator.push(x=x_t0, v=v_t0, B=B, E=E, q=1.0, m=1.0, dt=0.01)
+v_t0 = np.array([[0.0, 0.0, 500.0]])
+for n in range(100):
+    x_t0, v_t0 = BorisIntegrator.push(x=x_t0, v=v_t0, B=B, E=E, q=-1.6e-19, m=9.1e-31, dt=1e-5)
     print(x_t0)
